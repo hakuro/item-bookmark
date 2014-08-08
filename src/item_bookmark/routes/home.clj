154f83a389d10358bb6183cf530b0a34bb99c912
Naohisa Takahashi <hakuro16@gmail.com>
2014-08-09 01:50:37 +0900

    initial commit

---
+++ src/item_bookmark/routes/home.clj
@@ -0,0 +1,19 @@
+(ns item-bookmark.routes.home
+  (:require [item-bookmark.layout :as layout]
+            [item-bookmark.util :as util]
+            [compojure.core :refer :all]
+            [noir.response :refer [edn]]
+            [clojure.pprint :refer [pprint]]))
+
+(defn home-page []
+      (layout/render
+        "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn save-document [doc]
+      (pprint doc)
+      {:status "ok"})
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (POST "/save" {:keys [body-params]}
+    (edn (save-document body-params))))
